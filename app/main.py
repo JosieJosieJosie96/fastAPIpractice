33e5bc56b0bf421969b961c2e74ca0820135aedc
JosieJosieJosie96 <157616116+JosieJosieJosie96@users.noreply.github.com>
2025-05-29 14:56:34 +0300

    updated to start crud operations

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 # main.py
 
-from fastapi import FastAPI
+from typing import Optional, List
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+items: List[Item] = []
 app = FastAPI()
 
 @app.get("/")
@@ -11,3 +21,21 @@ def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    items.append(item)
+    return item
+
+@app.get("/items/", response_model=List[Item])
+async def get_items():
+    return items
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
